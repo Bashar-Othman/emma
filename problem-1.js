cf062225e532da0211d66832f5608810012131c0
Bashar-Othman <bashar.othman.message@gmail.com>
2021-12-17 22:49:47 +0200

    Add files via upload

---
+++ problem-1.js
@@ -0,0 +1,21 @@
+const example1 = [
+    "hello world",
+    "this is a test",
+    "this is an example"
+];
+
+function getCommonChars(arr) {
+    // Write your code here
+    let mainset = null;
+    let mainarr = "";
+    for (let entry of arr) {
+        mainarr+=entry;
+
+    }
+    console.log(mainarr);
+    mainset = new Set(mainarr);
+    console.log(mainset);
+}
+
+getCommonChars(example1);
+
